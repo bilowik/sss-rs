05142cf81213e2bdc821c9e8c67e20345e0d2a2e
Aaron Flores <aaronjflores962@gmail.com>
2019-09-11 16:36:10 -0400

    Added matrix transpose function and testing
    When creating shares from a large piece of data, a matrix is returned
    with each row containing the shares to one byte of data. When handing
    out shares of the data, you want each full share to contain one piece to
    each byte of data. Transposing the returned matrix makes this simple,
    with each row having a share for each byte of the secret.

--- src/share.rs
+++ src/share.rs
@@ -1,8 +1,6 @@
 use crate::geometry::*;
 use num_bigint_dig::{BigInt, BigUint, RandBigInt};
 use rand::rngs::{OsRng, StdRng};
-use rand::Rng;
-use rand::distributions::Distribution;
 use rand::SeedableRng;
 use std::ops::Rem;
 
@@ -66,6 +64,10 @@ pub fn create_share_lists_from_secrets(secret: &[u8], prime: &BigInt, shares_req
 
     let mut list_of_share_lists: Vec<Vec<Point>> = Vec::new();
 
+    for _ in 0..secret.len() {
+        list_of_share_lists.push(Vec::with_capacity(shares_to_create));
+    }
+
     for s in secret {
         match create_shares_from_secret(*s, 
                                            prime, 
@@ -74,6 +76,7 @@ pub fn create_share_lists_from_secrets(secret: &[u8], prime: &BigInt, shares_req
                                            co_max_bits,
                                            _x_value_max_bits) {
             Ok(shares) => {
+                // Now this list needs to be transposed:
                 list_of_share_lists.push(shares);
             },
             Err(_) => {
@@ -86,6 +89,12 @@ pub fn create_share_lists_from_secrets(secret: &[u8], prime: &BigInt, shares_req
 }
 
 
+
+/// The share lists should be in this format since this is how they would be distributed:
+/// Each Vec in Vec<Vec<Point>> should be: share1byte1, share1byte2, share1byte3 and so on.
+/// The first index is the share, the second being the byte of the share.
+/// This is also how they are outputted so it can easily be sent back through and decrypted without
+/// modification.
 pub fn reconstruct_secrets_from_share_lists(share_lists: Vec<Vec<Point>>, prime: &BigInt,
                                             shares_needed: usize) -> Result<Vec<u8>, ()> {
     let mut secrets: Vec<u8> = Vec::with_capacity(share_lists.len());
@@ -105,7 +114,28 @@ pub fn reconstruct_secrets_from_share_lists(share_lists: Vec<Vec<Point>>, prime:
 
 
 
+/// Returns the "matrix" as is if the rows don't have an equal number of columns
+pub fn transpose_vec_matrix<T: Clone>(matrix: &Vec<Vec<T>>) -> Result<Vec<Vec<T>>, ()> {
 
+    for i in 1..matrix.len() {
+        if matrix[i - 1].len() != matrix[i].len() {
+            return Err(());
+        }
+    }
+
+    let mut transpose: Vec<Vec<T>> = Vec::new();
+
+    for _ in 0..matrix[0].len() {
+        transpose.push(Vec::new());
+    }
+
+    for i in 0..matrix.len() {
+        for j in 0..matrix[i].len() {
+            transpose[j].push(matrix[i][j].clone());
+        }
+    }
+    Ok(transpose)
+}
 
 
 
@@ -113,13 +143,10 @@ pub fn reconstruct_secrets_from_share_lists(share_lists: Vec<Vec<Point>>, prime:
 #[cfg(test)]
 mod tests {
     use num_bigint_dig::RandPrime;
-    use num_bigint_dig::RandBigInt;
     use rand::SeedableRng;
     use rand::Rng;
     use super::*;
-    use signal_hook::register;
-    use backtrace::Backtrace;
-    use num_traits::Pow;
+    //use signal_hook::register;
     use rand::rngs::SmallRng;
 
 
@@ -129,12 +156,11 @@ mod tests {
     fn many_test() {
 
         let num_iters = 50;
-        let seed = 123;
 
         let mut rand = SmallRng::seed_from_u64(123u64);
 
-        for i in 0..num_iters {
-            let secret: u8 = (rand.gen_range(1, 256) as u8);
+        for _ in 0..num_iters {
+            let secret: u8 = rand.gen_range(1, 256) as u8;
             let shares_required = rand.gen_range(2, 10);
             let shares_to_create = shares_required + rand.gen_range(0, 6);
             let bit_size_co: usize = rand.gen_range(32, 65);
@@ -176,9 +202,58 @@ mod tests {
         let secret_decrypted = reconstruct_secret(shares, &prime, shares_required).unwrap();
         assert_eq!(secret, secret_decrypted);
 
-
     }
 
+    #[test]
+    fn transpose() {
+        let matrix = vec![
+            vec![1, 2, 3],
+            vec![4, 5, 6],
+            vec![7, 8, 9]
+        ];
+
+        let matrix1 = vec![
+            vec![1, 4, 7],
+            vec![2, 5, 8],
+            vec![3, 6, 9]
+        ];
+
+
+        let matrix2 = vec![
+            vec![1, 2, 3, 4],
+            vec![5, 6, 7, 8]
+        ];
+         
+        let matrix3 = vec![
+            vec![1, 5],
+            vec![2, 6],
+            vec![3, 7],
+            vec![4, 8]
+        ];
+
+        assert_eq!(transpose_vec_matrix(&matrix).unwrap(), matrix1);
+        assert_eq!(transpose_vec_matrix(&matrix2).unwrap(), matrix3);
+    }
+
+
+    /*
+    #[test]
+    fn large_data() {
+        let rand = SmallRng::seed_from_u64(123);
+        let secret = "Hello World";
+        let shares_required = 3;
+        let shares_to_create = 3;
+        let co_max_bits = 64;
+        let prime = rand.gen_prime(128);
+        let _x_value_max_bits = 128;
+
+        let share_lists = create_share_lists_from_secrets(secret.as_bytes(), prime.into(), 
+                          shares_required, shares_to_create, co_max_bits, _x_value_max_bits);
+        let recon_secret = 
+
+
+    }
+    */
 
 
 }
