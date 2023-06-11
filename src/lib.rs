7e83a612afdfa49dd8137dc7e432769528452fa4
= <=>
2023-06-11 13:56:12 -0400

    Create a prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ pub mod basic_sharing;
 #[allow(dead_code)] 
 mod geometry;
 pub mod wrapped_sharing;
+
+
+pub mod prelude {
+    pub use crate::wrapped_sharing::{Sharer, Reconstructor, share, reconstruct};
+}
